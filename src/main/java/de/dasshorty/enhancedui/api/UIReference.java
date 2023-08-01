21f098fcceb1cbb8bd4ccd10cc5c36b3f2749130
Anthony T <ungegrieftfirma@gmail.com>
2023-08-01 22:00:10 +0200

    feature(new methods): added new methods to make the inventory more customizable

--- src/main/java/de/dasshorty/enhancedui/api/UIReference.java
+++ src/main/java/de/dasshorty/enhancedui/api/UIReference.java
@@ -1,10 +1,21 @@
 package de.dasshorty.enhancedui.api;
 
 import net.kyori.adventure.text.Component;
+import org.bukkit.entity.Player;
 
 public interface UIReference {
 
   Component displayText();
+
   UIType type();
+
+  boolean animated();
+
+  /**
+   * @return if true items can be moved in other inventories that the player has open
+   * */
+  boolean allowItemMovementInOtherInventories();
+
+  void open(Player player);
 
 }
