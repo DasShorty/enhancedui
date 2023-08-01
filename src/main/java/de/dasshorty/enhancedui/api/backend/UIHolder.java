9e3b5c060656ef1fc02af34c757d323e075ba86a
Anthony T <ungegrieftfirma@gmail.com>
2023-08-01 21:59:30 +0200

    feature(added click logic): added functionality to onClick method

---
+++ src/main/java/de/dasshorty/enhancedui/api/backend/UIHolder.java
@@ -0,0 +1,103 @@
+package de.dasshorty.enhancedui.api.backend;
+
+import de.dasshorty.enhancedui.api.UIReference;
+import de.dasshorty.enhancedui.api.types.chest.ChestUI;
+import lombok.val;
+import net.kyori.adventure.text.Component;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.inventory.InventoryClickEvent;
+import org.bukkit.event.inventory.InventoryCloseEvent;
+import org.bukkit.event.inventory.InventoryMoveItemEvent;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class UIHolder implements Listener {
+
+  private Map<Player, UIReference> openUIs = new HashMap<>();
+
+  public synchronized void open(UIReference reference, Player player) {
+    openUIs.put(player, reference);
+    reference.open(player);
+  }
+
+  public boolean isPlayerInUI(Player player) {
+    return openUIs.containsKey(player);
+  }
+
+  public UIReference getUi(Player player) {
+    if (!isPlayerInUI(player))
+      return null;
+
+    return openUIs.get(player);
+  }
+
+  @EventHandler
+  public void onInventoryClose(InventoryCloseEvent event) {
+
+    if (!(event.getPlayer() instanceof Player player))
+      return;
+
+    if (!isPlayerInUI(player))
+      return;
+
+    val uiReference = getUi(player);
+
+    switch (uiReference.type()) {
+      case CHEST -> {
+
+        val ui = (ChestUI) uiReference;
+
+        if (event.getInventory().getHolder() == ui.getInventory().getHolder())
+          openUIs.remove(player);
+
+
+      }
+    }
+
+  }
+
+  @EventHandler
+  public void onInventoryClick(InventoryClickEvent event) {
+
+    if (!(event.getWhoClicked() instanceof Player player))
+      return;
+
+    if (!isPlayerInUI(player))
+      return;
+
+    val uiReference = getUi(player);
+
+
+    switch (uiReference.type()) {
+      case CHEST -> {
+
+        val ui = (ChestUI) uiReference;
+
+        if (event.getInventory().getHolder() != ui.getInventory().getHolder())
+          return;
+
+        if (event.getClickedInventory() == null)
+          return;
+
+        if (!ui.allowItemMovementInOtherInventories())
+          if (event.getClickedInventory().getHolder() != ui.getInventory().getHolder())
+            return;
+
+        ui.getItemsMap().forEach((uiItem, uiAction) -> {
+
+          if (uiItem.getCurrentSlot().getSlot() != event.getSlot())
+            return;
+
+          event.setCancelled(uiAction.onItemClick(player, uiItem, event.getClick()));
+
+        });
+
+      }
+    }
+
+  }
+
+}
--- src/main/java/de/dasshorty/enhancedui/api/types/chest/ChestUI.java
+++ src/main/java/de/dasshorty/enhancedui/api/types/chest/ChestUI.java
@@ -1,10 +1,15 @@
 package de.dasshorty.enhancedui.api.types.chest;
 
 import de.dasshorty.enhancedui.api.UIReference;
+import de.dasshorty.enhancedui.api.UIRow;
+import de.dasshorty.enhancedui.api.UISlot;
 import de.dasshorty.enhancedui.api.UIType;
+import de.dasshorty.enhancedui.api.background.UIBackground;
 import de.dasshorty.enhancedui.api.exception.OutOfInventoryException;
-import de.dasshorty.enhancedui.api.UIAction;
-import de.dasshorty.enhancedui.api.UIItem;
+import de.dasshorty.enhancedui.api.item.UIAction;
+import de.dasshorty.enhancedui.api.item.UIItem;
+import lombok.Getter;
+import lombok.val;
 import net.kyori.adventure.text.Component;
 import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
@@ -13,37 +18,104 @@ import org.bukkit.inventory.InventoryHolder;
 import org.jetbrains.annotations.NotNull;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public abstract class ChestUI implements InventoryHolder, UIReference {
 
+  @Getter
   protected final Map<UIItem, UIAction> itemsMap = new HashMap<>();
+  protected final UIRow rows;
   private final Component uiTitle;
   private final Inventory inventory;
-  protected final ChestUIRow rows;
+  private UIBackground background;
 
-  public ChestUI(Component uiTitle, ChestUIRow slots, Player opener) {
+  public ChestUI(Component uiTitle, UIRow slots, Player opener) {
     this.uiTitle = uiTitle;
     this.rows = slots;
     this.inventory = Bukkit.createInventory(this, slots.getSlots(), uiTitle);
 
-    initItems(opener);
-    refreshInventory();
+    background = new UIBackground(UIBackground.BackgroundType.NONE, List.of());
+
+    try {
+      initItems(opener);
+    } catch (OutOfInventoryException e) {
+      throw new RuntimeException(e);
+    }
   }
 
-  protected abstract void initItems(Player opener);
+  @Override
+  public void open(Player player) {
+    try {
+      refreshInventory();
+    } catch (OutOfInventoryException e) {
+      throw new RuntimeException(e);
+    }
+    player.openInventory(inventory);
+  }
 
-  protected void refreshInventory() {
+  protected abstract void initItems(Player opener) throws OutOfInventoryException;
+
+  public void setBackground(UIBackground background) {
+    this.background = background;
+  }
+
+  protected void refreshInventory() throws OutOfInventoryException {
     itemsMap.forEach((item, uiAction) -> this.inventory.setItem(item.getOriginSlot().getSlot(), item.buildItem()));
-  }
 
+    for (int i = 0; i < this.rows.getSlots(); i++) {
+
+      val item = inventory.getItem(i);
+
+      if (background.type() == UIBackground.BackgroundType.NONE)
+        continue;
+
+      if (item == null) {
+
+        switch (background.type()) {
+          case FULL -> {
+
+            val uiItem = background.backgroundItems().get(0);
+            item(new UIItem(uiItem.buildItem(), UISlot.fromSlotId(i).get()), (clicker, clickedItem, action) -> true);
+
+          }
+          case SELF -> {
+
+            if (background.backgroundItems().size() < this.rows.getSlots()) {
+
+              if (i != 0) {
+
+                val uiItem = background.backgroundItems().get(0);
+                item(new UIItem(uiItem.buildItem(), UISlot.fromSlotId(i).get()), (clicker, clickedItem, action) -> true);
+
+              }
+
+              throw new IllegalArgumentException("Not enough items for a self background in the list. Required: " + this.rows.getSlots() + " got: " + background.backgroundItems().size() + " items");
+
+            } else {
+              val uiItem = background.backgroundItems().get(i);
+              item(new UIItem(uiItem.buildItem(), UISlot.fromSlotId(i).get()), (clicker, clickedItem, action) -> true);
+            }
+
+          }
+        }
+
+      }
+
+    }
+  }
 
   public void item(UIItem item, UIAction click) throws OutOfInventoryException {
 
-    if (!ChestUIRow.isParentFrom(rows, item.getOriginSlot().getParent()))
+    if (!UIRow.isParentFrom(rows, item.getOriginSlot().getParent()))
       throw new OutOfInventoryException(this, item, rows);
 
     itemsMap.put(item, click);
+  }
+
+  @Override
+  public boolean animated() {
+    return false;
   }
 
   @Override
