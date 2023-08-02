8cb17641ee3e93616e66170f1977d3bec26545c2
Anthony T <ungegrieftfirma@gmail.com>
2023-08-02 14:28:40 +0200

    refactor(removed plugin.yml)

--- build.gradle.kts
+++ build.gradle.kts
@@ -42,17 +42,6 @@ tasks {
   }
   processResources {
     filteringCharset = Charsets.UTF_8.name() // We want UTF-8 for everything
-
-    val props = mapOf(
-      "name" to project.name,
-      "version" to project.version,
-      "description" to project.description,
-      "apiVersion" to "1.20"
-    )
-    inputs.properties(props)
-    filesMatching("plugin.yml") {
-      expand(props)
-    }
   }
 
 
